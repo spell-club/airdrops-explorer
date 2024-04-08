4d2a65c94d65dd92bdb9a006f1227887b596685b
Mikhail Rypta <mikhailrypta@Mikhails-MacBook-Pro.local>
2024-04-08 20:43:15 +0300

    removed unused code

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,9 +3,9 @@ import celestia from '../../public/img/airdrops/celestia.webp'
 import neutron from '../../public/img/airdrops/neutron.webp'
 
 const AIRDROPS_IMAGES: Record<string, string> = {
-  'osmosis': osmosis.src,
-  'celestia': celestia.src,
-  'neutron': neutron.src,
+  osmosis: osmosis.src,
+  celestia: celestia.src,
+  neutron: neutron.src,
 }
 
 export { AIRDROPS_IMAGES }
