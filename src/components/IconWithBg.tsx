46fff7d81dd34ab577b493db67f2c4599dcc23d8
Mikhail Rypta <m.rypta@everstake.one>
2024-04-10 15:00:35 +0300

    added husky

--- src/components/IconWithBg.tsx
+++ src/components/IconWithBg.tsx
@@ -5,18 +5,22 @@ interface Props {
 	icon: string
 }
 
-const IconWithBg = chakra(({ className, icon }: Props) => (
-	<Flex
-		p={0.5}
-		className={className}
-		justify="center"
-		align="center"
-		borderRadius="50%"
-		bg="black"
-		zIndex="1"
-	>
-		<Image borderRadius="50%" w="100%" src={icon} />
-	</Flex>
-))
+const IconWithBg = chakra(({ className, icon }: Props) => {
+	var a = 3
+
+	return (
+		<Flex
+			p={0.5}
+			className={className}
+			justify="center"
+			align="center"
+			borderRadius="50%"
+			bg="black"
+			zIndex="1"
+		>
+			<Image borderRadius="50%" w="100%" src={icon} alt="icon" />
+		</Flex>
+	)
+})
 
 export default IconWithBg
