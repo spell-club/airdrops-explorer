413ebf17f498c034d001f8f6acc00d0342c55ccb
Mikhail Rypta <m.rypta@everstake.one>
2024-04-24 12:28:59 +0300

    add loader bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import { ReactNode } from 'react'
-import AppWrappers from './AppWrappers'
+import NextTopLoader from 'nextjs-toploader'
+import type { Metadata } from 'next'
+
 import Head from './head'
 
-import type { Metadata } from 'next'
+import AppWrappers from './AppWrappers'
 
 export const metadata: Metadata = {
 	title: 'SpellDrop',
@@ -15,7 +17,15 @@ export default function RootLayout({ children }: { children: ReactNode }) {
 			<Head />
 
 			<body id="root" className="chakra-ui-dark" suppressHydrationWarning={true}>
-				<AppWrappers>{children} </AppWrappers>
+				<NextTopLoader
+					color="#A278E1"
+					easing="ease"
+					speed={200}
+					shadow="0 0 10px #A278E1,0 0 5px #A278E1"
+					showSpinner
+				/>
+
+				<AppWrappers>{children}</AppWrappers>
 			</body>
 		</html>
 	)
