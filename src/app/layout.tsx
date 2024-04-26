9b73c691d7074fe894828b904b385e37fdcf9690
Mikhail Rypta <m.rypta@everstake.one>
2024-04-26 16:42:47 +0300

    used theme color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from 'react'
 import NextTopLoader from 'nextjs-toploader'
 import type { Metadata } from 'next'
 
+import { globalStyles } from 'styles/theme/styles'
+
 import Head from './head'
 
 import AppWrappers from './AppWrappers'
@@ -12,16 +14,18 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+	const loaderColor = globalStyles.colors.purple[100]
+
 	return (
 		<html lang="en">
 			<Head />
 
 			<body id="root" className="chakra-ui-dark" suppressHydrationWarning={true}>
 				<NextTopLoader
-					color="#A278E1"
+					color={loaderColor}
 					easing="ease"
 					speed={200}
-					shadow="0 0 10px #A278E1,0 0 5px #A278E1"
+					shadow={`0 0 10px ${loaderColor},0 0 5px ${loaderColor}`}
 					showSpinner
 				/>
 
